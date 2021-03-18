97ed5dd9165f4c337eab99c57170684d4888de77
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-18 03:54:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,3 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  BRANDS = []
-
-# It only wants you to add a brand IF the brand has not already been added to the array
-  def initialize(brand)
-    @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand)
-  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
-  def brand=(brand)
-    BRANDS << brand
-  end
 
 end
